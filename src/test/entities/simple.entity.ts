d7e2902fea91c7133b2acc5a33c800cbf74ed7ce
Luca Roverelli <luca.roverelli@gmail.com>
2022-01-14 09:44:07 +0100

    fixed issue with generating metadata for entities

--- src/test/entities/simple.entity.ts
+++ src/test/entities/simple.entity.ts
@@ -1,5 +1,5 @@
 import { AdminField, AdminType, AdminUI } from '../../decorators';
-import EditableEntity, { map } from '../../editable-entity';
+import EditableEntity, { map, notEditableFromPopup } from '../../editable-entity';
 import BaseEntity from 'lynx-framework/entities/base.entity';
 import {
   Entity,
@@ -39,7 +39,7 @@ export default class Simple extends BaseEntity implements EditableEntity
     selfType: 'Complex',
     inverseSide: 'simple',
     values: async () => map(await Complex.find()),
-    hide: true,
+    readOnly: notEditableFromPopup,
   })
   @ManyToOne(() => Complex, complex => complex.simple)
   complex: Complex;
