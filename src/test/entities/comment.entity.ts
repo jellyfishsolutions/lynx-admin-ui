9953e6fe6f9f3094d147e5e01f7658869533e128
Luca Roverelli <luca.roverelli@gmail.com>
2022-03-15 10:48:35 +0100

    added support for "readonly" property in the action-button type

--- src/test/entities/comment.entity.ts
+++ src/test/entities/comment.entity.ts
@@ -1,38 +1,64 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { AdminUI, AdminField, AdminType } from '../../decorators';
-import BaseEntity from "lynx-framework/entities/base.entity";
-import EditableEntity, { notEditableFromPopup } from "../../editable-entity";
-import {map} from "../../editable-entity";
-import Post from "./post.entity";
+import BaseEntity from 'lynx-framework/entities/base.entity';
+import EditableEntity, { notEditableFromPopup } from '../../editable-entity';
+import { map } from '../../editable-entity';
+import Post from './post.entity';
 
-@Entity("comments")
-@AdminUI("Comment", {
-    disableCreation: true
+@Entity('comments')
+@AdminUI('Comment', {
+    disableCreation: true,
 })
 export default class Comment extends BaseEntity implements EditableEntity {
     @PrimaryGeneratedColumn()
-    @AdminField({ name: "Id", type: AdminType.Id, readOnly: true, onSummary: true })
+    @AdminField({
+        name: 'Id',
+        type: AdminType.Id,
+        readOnly: true,
+        onSummary: true,
+    })
     id: number;
 
     @Column()
-    @AdminField({ name: "Text", type: AdminType.RichText, onSummary: true, searchable: true, uiSettings: { listTemplate: '/safe' } })
+    @AdminField({
+        name: 'Text',
+        type: AdminType.RichText,
+        onSummary: true,
+        searchable: true,
+        uiSettings: { listTemplate: '/safe' },
+    })
     text: string;
 
-    @ManyToOne(type => Post, post => post.comments, { eager: true })
-    @AdminField({ name: "Post", selfType: 'Post', type: AdminType.Selection, readOnly: notEditableFromPopup, values: async () => map(await Post.find())})
+    @ManyToOne((type) => Post, (post) => post.comments, { eager: true })
+    @AdminField({
+        name: 'Post',
+        selfType: 'Post',
+        type: AdminType.Selection,
+        readOnly: notEditableFromPopup,
+        values: async () => map(await Post.find()),
+    })
     post: Post;
-
 
     @Column()
     display: boolean = true;
 
-    @AdminField({ name: 'I am a BUTTON', type: AdminType.ActionButton, uiSettings: { innerEditorClasses: 'btn btn-warning' }, hide: async (_, k) => k.display  })
+    @AdminField({
+        name: 'I am a BUTTON',
+        type: AdminType.ActionButton,
+        uiSettings: { innerEditorClasses: 'btn btn-warning' },
+        hide: async (_, k) => k.display,
+    })
     async actionButton() {
         console.log("I'm a function!!");
         this.display = true;
     }
 
-    @AdminField({ name: 'I am a BUTTON 2', type: AdminType.ActionButton, uiSettings: { innerEditorClasses: 'btn btn-danger' }, hide: async (_, k) => !k.display  })
+    @AdminField({
+        name: 'I am a BUTTON 2',
+        type: AdminType.ActionButton,
+        uiSettings: { innerEditorClasses: 'btn btn-danger' },
+        readOnly: async (_, k) => !k.display,
+    })
     async actionButton2() {
         console.log('sono una 2');
         this.display = false;
@@ -43,11 +69,10 @@ export default class Comment extends BaseEntity implements EditableEntity {
     }
 
     getLabel(): string {
-        let c = this.text || "";
+        let c = this.text || '';
         if (c.length < 20) {
             return c;
         }
-        return c.substring(0, 20) + "...";
+        return c.substring(0, 20) + '...';
     }
-
 }
