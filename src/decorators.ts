0b9cd22057df1a0678ba2fd5e830b9e37798137e
Gabriele Zereik <gabriele.zereik@jellyfishsolutions.it>
2022-03-15 10:58:53 +0100

    fix signature of  hasRightColumn property

--- src/decorators.ts
+++ src/decorators.ts
@@ -46,7 +46,7 @@ export interface UISettings {
 export interface EntityUISettings {
     tabs?:  { key: string; label: string }[] | ((req: Request) => Promise< { key: string; label: string }[]>);
     defaultTab?: string | ((req: Request) => Promise<string>);
-    hasRightColumn?: boolean| ((req: Request, currentEntity: any) => Promise<boolean>);
+    hasRightColumn?: boolean| ((req: Request) => Promise<boolean>);
 }
 
 export interface ClassParameters {
